20209382a73e2c9f61915ae943407af6f2fa6050
Pragati Singh <pragatisinghshreya8303@gmail.com>
2024-11-21 18:47:36 +0530

    small change

--- src/app/dashboard/review/page.jsx
+++ src/app/dashboard/review/page.jsx
@@ -62,7 +62,7 @@ export default function Review (){
               showAnchorIcon
               href={card.link}
             >
-              Order Foods
+              Order Food
             </Link>
           </CardFooter>
         </Card>
