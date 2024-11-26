176264cb8c6e75630faada9d56f08a8ac7102139
Pragati Singh <pragatisinghshreya8303@gmail.com>
2024-11-26 19:54:31 +0530

    change

--- src/app/dashboard/review/page.jsx
+++ src/app/dashboard/review/page.jsx
@@ -62,7 +62,7 @@ export default function Review (){
               showAnchorIcon
               href={card.link}
             >
-              Order Food
+              Order Foods
               {/* db.user.find({
                 $expr:{
                   $gt:["$price",{$avg:"$price"}]
