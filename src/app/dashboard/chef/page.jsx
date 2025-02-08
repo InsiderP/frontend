be76422a34780b4666593b0417fc7b1661220922
Pragati Singh <pragatisinghshreya8303@gmail.com>
2025-02-08 11:37:33 +0530

    changes50

--- src/app/dashboard/chef/page.jsx
+++ src/app/dashboard/chef/page.jsx
@@ -74,7 +74,7 @@ export default function Chef() {
           years_of_experience: 5,
         },
         {
-          title: "ghi",
+          title: "ghiuuuuu",
           img: "https://images.pexels.com/photos/3456789/pexels-photo-3456789.jpeg?auto=compress&cs=tinysrgb&w=800",
           rating: 3,
           years_of_experience: 5,
